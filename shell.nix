89ed2ef0b90dfdd4bbed344497b068c5b868af8c
Chad Boyce <chadboyce@gmail.com>
2023-01-13 06:53:24 +0000

    addrd working shell.nix 🧃

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {
+    overlays = [
+      (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+    ];
+  }
+}:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    rust-bin.stable.latest.minimal
+    llvmPackages.bintools
+    gmp5
+    glibc
+  ];
+}
